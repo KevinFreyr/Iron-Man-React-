75b2ee5a2532e4099f79497e8f2ca33d6539be47
Kristjanjoelk <kristjank14@ru.is>
2017-01-17 17:13:09 +0000

    Start of handleClick

--- src/questions/questions.js
+++ src/questions/questions.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import './questions.css';
 class Questions extends Component {
+
     render() {
+        var results = 0;
+        var handleClick = function() {
+            results = results + 1;
+        }
         return (
             <div className="Questions-container">
             <div className="Questions-options">Question 1</div>
@@ -10,7 +15,10 @@ class Questions extends Component {
             <div className="Questions-options">Answer 4</div>
             <div className="Questions-options">Answer 5</div>
             <div className="Questions-options">Answer 6</div>
-            <div className="Questions-button-container"> </div>
+            <div className="Questions-button-container"> 
+                <button>Senda</button> <button>Hreinsa</button> <button onClick={handleClick}>test</button>
+            </div>
+            <div> result: {results} </div>
             </div>
         );
     }
