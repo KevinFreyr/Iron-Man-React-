eaf246a71bb564fc7c5d404eb6e14ea8021e4a2d
Kristjanjoelk <kristjank14@ru.is>
2017-01-17 16:07:20 +0000

    Questions class added

---
+++ src/questions/questions.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Questions extends Component {
+    render() {
+        return (
+            <div>
+            <div>Question 1</div>
+            <div>Answer 1</div>
+            <div>Answer 2</div>
+            <div>Answer 3</div>
+            <div>Answer 4</div>
+            </div>
+        );
+    }
+}
+
+export default Questions;
